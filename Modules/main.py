1f8ca9cafd9650d71a32b23fe6c8635a2720a0b5
hydrenoid <moore.johnny27@gmail.com>
2024-02-07 14:52:39 -0700

    Outlined main application

---
+++ Modules/main.py
@@ -0,0 +1,33 @@
+#todo imports
+
+
+#todo Run diagnostic on hardware
+
+#todo User chooses what areas will have plants
+
+#todo are there areas not being used? (yes set those areas to off)
+
+#todo Check water and food level
+
+#todo are they full? (no prompt user to fill and wait for completion)
+
+#todo show user how to plant their seeds
+
+# Open main loop
+while True:
+    continue
+    #todo Check schedule configuration
+
+    #todo is it time to water? (yes turn pumps on/keep on; no turn off)
+
+    #todo is it time for light? (yes turn light on/keep on; no turn off)
+
+    #todo Take picture of plants and sparse out each one specifically
+
+    #todo Process images and get health percentage
+
+    #todo Display health percentage to user
+
+    #todo is health < 80%? (yes, send notification to user)
+
+    #todo wait five minutes
