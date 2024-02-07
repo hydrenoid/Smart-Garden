5e99b61b33764209cb1bec6edbb30751d9ed7e52
hydrenoid <moore.johnny27@gmail.com>
2024-02-07 11:27:16 -0700

    First Commit!

---
+++ Modules/camera.py
@@ -0,0 +1,53 @@
+
+
+
+
+#TODO Run diagnostic on hardware
+
+#TODO User chooses what plants will be there
+
+#TODO Assign each spot to their respective plant
+
+#TODO check for empty spots (yes set spaces to neutral, no continue)
+
+#TODO check if water and food are full (no prompt user to fill and wait, yes continue)
+
+#TODO Show user how to plant their seeds and wait for user confirmation
+
+#TODO Show user expected harvest dates
+
+# Open Main Loop
+while True:
+    continue
+    #TODO Check plant configuration
+
+    #TODO is it time to water? (yes turn pumps on for time then turn them off, no continue)
+
+    #TODO is it time for light? (yes turn on light for time then turn off, no continue)
+
+    #TODO is the temp in range? (no notify user and provide temp range, yes continue)
+
+    #TODO time for harvest?
+
+    # NO
+        #TODO Take picture of plants and sparse out each one
+
+        #TODO run picture through the NDVI Model
+
+        #TODO Is plant health > 80%? (no alert user and give them tips to fix, yes continue)
+
+        #TODO Check water and food resorvoirs
+
+        #TODO if anything needs refilled alert user to refill what is needed
+
+    # YES
+        #TODO Notify user to harvest plant
+
+    #TODO Record status and data for training
+
+    #TODO Move to next plant
+
+
+
+
+
