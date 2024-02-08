eb4d17289e2fc64c49a7f4c5a5ba60be0d2e3110
hydrenoid <moore.johnny27@gmail.com>
2024-02-07 18:03:59 -0700

    Outlined main application

--- Modules/camera.py
+++ Modules/camera.py
@@ -1,53 +1,84 @@
+import cv2
+import numpy as np
+import time
+from fastiecm import fastiecm
+from picamera2 import Picamera2, Preview
+
+
+def display(image, image_name):
+    image = np.array(image, dtype=float)/float(255)
+    shape = image.shape
+    height = int(shape[0] / 2)
+    width = int(shape[1] / 2)
+    image = cv2.resize(image, (width, height))
+    cv2.namedWindow(image_name)
+    cv2.imshow(image_name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def contrast_stretch(im):
+    in_min = np.percentile(im, 5)
+    in_max = np.percentile(im, 95)
+
+    out_min = 0.0
+    out_max = 255.0
+
+    out = im - in_min
+    out *= ((out_min - out_max) / (in_min - in_max))
+    out += in_min
+
+    return out
+
+
+def calc_ndvi(image):
+    b, g, r = cv2.split(image)
+    bottom = (r.astype(float) + b.astype(float))
+    bottom[bottom==0] = 0.01
+    ndvi = (r.astype(float) - b) / bottom # THIS IS THE CHANGED LINE
+    return ndvi
 
 
 
+#TODO: Takes a picture and saves it to the file "/Smart-Garden/Images/Originals"
+def take_picture():
+    picam2 = Picamera2()
+    camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (1920, 1080)},
+                                                      display="lores")
+    picam2.configure(camera_config)
+    picam2.start()
+    time.sleep(1)
 
-#TODO Run diagnostic on hardware
+    original = picam2.capture_array("main")
 
-#TODO User chooses what plants will be there
+    timestr = time.strftime("%Y%m%d-%H%M%S")
 
-#TODO Assign each spot to their respective plant
+    cv2.imwrite('Images/Originals/original_' + timestr, original)
 
-#TODO check for empty spots (yes set spaces to neutral, no continue)
-
-#TODO check if water and food are full (no prompt user to fill and wait, yes continue)
-
-#TODO Show user how to plant their seeds and wait for user confirmation
-
-#TODO Show user expected harvest dates
-
-# Open Main Loop
-while True:
-    continue
-    #TODO Check plant configuration
-
-    #TODO is it time to water? (yes turn pumps on for time then turn them off, no continue)
-
-    #TODO is it time for light? (yes turn on light for time then turn off, no continue)
-
-    #TODO is the temp in range? (no notify user and provide temp range, yes continue)
-
-    #TODO time for harvest?
-
-    # NO
-        #TODO Take picture of plants and sparse out each one
-
-        #TODO run picture through the NDVI Model
-
-        #TODO Is plant health > 80%? (no alert user and give them tips to fix, yes continue)
-
-        #TODO Check water and food resorvoirs
-
-        #TODO if anything needs refilled alert user to refill what is needed
-
-    # YES
-        #TODO Notify user to harvest plant
-
-    #TODO Record status and data for training
-
-    #TODO Move to next plant
+    #todo process the image
+    health = process_image(original, timestr)
 
 
+#TODO: Sparse out the plants from the rest of picture
+def photo_sparse():
+    print('Parsing out pictures.')
 
+
+#TODO: Process image and return a health percentage
+def process_image(original, timestr):
+    contrasted = contrast_stretch(original)
+
+    ndvi = calc_ndvi(contrasted)
+
+    ndvi_contrasted = contrast_stretch(ndvi)
+
+    color_mapped_prep = ndvi_contrasted.astype(np.uint8)
+    color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
+
+    cv2.imwrite('Images/Originals/original_' + timestr, color_mapped_image)
+
+    # TODO: generate health index from image
+    health = 0
+    return health
 
 
--- Modules/console.py
+++ Modules/console.py
@@ -9,11 +9,18 @@ layout = [[sg.Text("What's your name?")],
 
 # Create the window
 window = sg.Window('Window Title', layout)
+window2 = sg.Window('Window Title2', layout)
 
 # Display and interact with the Window using an Event Loop
 while True:
     event, values = window.read()
+    event2, values2 = window2.read()
     # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    # Output a message to the window
+    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
+
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
     # Output a message to the window
@@ -21,3 +28,23 @@ while True:
 
 # Finish up by removing from the screen
 window.close()
+window2.close()
+
+#TODO: This method initializes the windows to be displayed and shows them
+def initialize_windows():
+    True
+
+
+#TODO: This method closes the windows
+def close_windows():
+    True
+
+
+#TODO: This method receives a notification with inputs ("be skipped?" bool, "Title" string, "Description" string)
+def check_notifications(skippable, title, description):
+    True
+
+
+#TODO: This method removes a certain notification.
+def rem_notification(int):
+    True
